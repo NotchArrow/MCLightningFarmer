69564e00aea903663d5266d484d9c83c5011f69a
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2024-08-21 07:21:06 -0400

    Update LightningFarm.py

--- LightningFarm.py
+++ LightningFarm.py
@@ -3,19 +3,21 @@ import pyautogui
 from time import sleep
 
 PATH = os.path.expanduser("~/AppData/Roaming/.minecraft/logs/latest.log")
-log_file = open(PATH, "r")
 storming = False
 singleplayer = True
 refresh_interval = 10
 
 while not storming:
 	sleep(refresh_interval)
+	log_file = open(PATH, "r")
 	log_file_lines = log_file.readlines()
 	for log_file_line in log_file_lines:
 		if "Cat was struck by lightning" in log_file_line:
 			storming = True
 			break
 
+	log_file.close()
+	
 	if storming:
 		if singleplayer:
 			pyautogui.press('esc')
